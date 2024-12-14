a9b1b619ac5005c769187b8eb112647cb4f9d1ec
RAJNISH KUMAR SHARMA <163850553+RajnisSharma@users.noreply.github.com>
2024-12-14 18:22:34 +0530

    Update client.js

--- client.js
+++ client.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://zccxgaswklkkpmpxgluo.supabase.co";
+const supabaseUrl = "";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpjY3hnYXN3a2xra3BtcHhnbHVvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM5MTIwMDYsImV4cCI6MjA0OTQ4ODAwNn0.Gs3BYF_MMdpJDxodqLh9Hc8LbAk1s0fzzohW5WkoSaE";
+  "";
 const CDNURL =
   "https://zccxgaswklkkpmpxgluo.supabase.co/storage/documents/file/";
 

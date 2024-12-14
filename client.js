b9e41441c83fa9a8ee3a004c1922235d8ddbaaf9
RAJNISH KUMAR SHARMA <163850553+RajnisSharma@users.noreply.github.com>
2024-12-14 18:22:55 +0530

    Update client.js

--- client.js
+++ client.js
@@ -1,9 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "";
-const supabaseKey =
-  "";
-const CDNURL =
-  "https://zccxgaswklkkpmpxgluo.supabase.co/storage/documents/file/";
+const supabaseKey = "";
+const CDNURL = "https://zccxgaswklkkpmpxgluo.supabase.co/storage/documents/file/";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
